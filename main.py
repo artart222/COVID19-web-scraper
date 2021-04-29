3b381ab65db2ccd317859e00320cdb89515cda25
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-04-29 15:38:30 +0430

    now program show data correctly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
 
 URL = "https://www.worldometers.info/coronavirus/countries-where-coronavirus-has-spread/"
 r = requests.get(URL)
@@ -12,8 +11,9 @@ number_of_data = 0
 
 for tag in soup.findAll("tr"):
     #  stag = second tag
+    list_of_data = []
     for stag in tag.findAll("td"):
-        list_of_data = []
+
         if number_of_data % 4 == 0:
             country = stag.getText()
         elif number_of_data % 4 == 1:
@@ -25,3 +25,6 @@ for tag in soup.findAll("tr"):
             data[country] = list_of_data
 
         number_of_data += 1
+
+for keys, values in data.items():
+    print(keys,  "==> ", "Total cases:", values[0], "Total deaths", values[1])
