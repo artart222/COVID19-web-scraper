f54fd10937d110e6b47f8b7a677099c019d41a7a
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-04-29 15:53:58 +0430

    save to file option added to codes

--- main.py
+++ main.py
@@ -27,4 +27,12 @@ for tag in soup.findAll("tr"):
         number_of_data += 1
 
 for keys, values in data.items():
-    print(keys,  "==> ", "Total cases:", values[0], "Total deaths", values[1])
+    print(keys, "==> ", "Total cases:", values[0], "Total deaths:", values[1])
+
+
+save = input("\n\nDo you want save this statistics to a file (yes/no): ")
+if save == "yes":
+    with open("covid19-statistics.txt", "w") as f:
+        for keys, values in data.items():
+            string_data = keys + " ==> " + "Total cases: " + values[0] + " Total deaths: " + values[1] + "\n"
+            f.write(string_data)
